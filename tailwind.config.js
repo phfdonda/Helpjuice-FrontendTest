3eff7f5a8f7b48920ea8119a7ffec3c8eae5f32c
phfdonda <phfdonda@gmail.com>
2024-03-20 10:59:30 -0300

    Style dropdown menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
  theme: {
   fontFamily: {
    title: ["Plus Jakarta Sans", "sans-serif"],
+   fun: ["Alphabetized Cassette Tapes Thin", "cursive"],
    sans: ["Open Sans", "sans-serif"],
   },
  },
